6b3dfb0e716fea60c4db1db169d3653e06297b15
Joao Reis <joaoteireis@gmail.com>
2024-08-13 16:31:58 -0300

    creating the pieces package and the character class

---
+++ src/pieces/Characters.java
@@ -0,0 +1,43 @@
+package pieces;
+
+public class Characters {
+    private int life;
+    private int defense;
+    private int attack;
+    private String name;
+
+    public Characters() {
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
